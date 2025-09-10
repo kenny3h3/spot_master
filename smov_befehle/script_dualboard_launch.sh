dc7e108842efeca9d37b530e556e924eec25b76c
kenny3h3 <129788071+kenny3h3@users.noreply.github.com>
2025-09-10 11:26:28 +0200

    Imu Launch

--- smov_befehle/script_dualboard_launch.sh
+++ smov_befehle/script_dualboard_launch.sh
@@ -9,3 +9,6 @@ source /home/winston/spot_ws/install/setup.bash
 echo "ROS dualboard launch"
 cd /home/winston/spot_ws
 ros2 launch smov_bringup dual_board.launch.py
+
+echo "IMU launch"
+ros2 launch bno085_imu.py imu.launch.py
